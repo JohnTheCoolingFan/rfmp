3848ab08b6d48c28b57e83bb10fd50ae97e2c2c7
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-11-17 10:32:08 +0300

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, env, io::copy};
 use std::path::{Path, PathBuf};
-use std::time::{Instant};
+use std::time::Instant;
 use zip::CompressionMethod;
 use zip::write::{ZipWriter, FileOptions};
 use serde_json::{from_reader, Value};
